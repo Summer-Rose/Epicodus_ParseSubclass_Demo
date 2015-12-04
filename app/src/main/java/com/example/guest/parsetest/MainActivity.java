d0f9d6dca96eee554e0b8011ade0079d748a8e82
Summer Brochtrup <summerbrochtrup@Summers-MacBook-Pro.local>
2015-12-04 10:08:39 -0800

    Add listview and arraylist adapter

--- app/src/main/java/com/example/guest/parsetest/MainActivity.java
+++ app/src/main/java/com/example/guest/parsetest/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.guest.parsetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -11,29 +12,36 @@ import com.parse.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText kittenNameEditText = (EditText) findViewById(R.id.kittenNameEditText);
+
         findViewById(R.id.createKittenButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Kitten meowsers = new Kitten("meowsers");
-                meowsers.saveInBackground();
+                String name = kittenNameEditText.getText().toString();
+                Kitten newKitten = new Kitten(name);
+                newKitten.saveInBackground();
+                adapter.add(name);
+                kittenNameEditText.setText("");
             }
         });
 
         Kitten.findAll(new FindCallback<Kitten>() {
             @Override
             public void done(List<Kitten> kittens, ParseException e) {
-                TextView kittenListText = (TextView) findViewById(R.id.kittenListText);
                 ArrayList<String> kittenNames = new ArrayList<String>();
                 for (int i = 0; i < kittens.size(); i++) {
                     kittenNames.add(kittens.get(i).getString("name"));
                 }
-                kittenListText.setText(kittenNames.toString());
+                adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, kittenNames);
+                setListAdapter(adapter);
             }
         });
 
